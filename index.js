093807dcb347b7643d386b6d9cfd2eb126853823
robertsevernsentient <robert.severn@evolv.ai>
2024-01-23 14:56:00 -0800

    Add a basic handler for events on matching a url

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 module.exports = function(config) {
     console.log('Integrations run', config)
+
+    const urlEmitter = (url) => {
+        if (url === 'https://www.evolv.ai/') {
+            evolv.emit('checkout');
+        }
+    }
+
+    evolv.on('context.value.changed', function (key, value) {
+        if (key === 'web.url') {
+            urlEmitter(value);
+        }
+    });
 };
